b580ef6e16ce080d79e763fb01acb80dbeb49370
Sebastian Riedel <git@basxto.de>
2018-09-30 00:15:53 +0200

    Seperate head and head shadow

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-for file in male_drake_head.png male_human_head.png male_ogre_head.png;do
+for file in male_drake_head.png male_drake_head_shadow.png male_human_head.png male_human_head_shadow.png male_ogre_head.png male_skeleton_head.png;do
     ./lpc-shell-tools/duplimap.sh ${file} ./lpc-shell-tools/animation/head/male/walkcycle.map.csv 64 64
 done
---
+++ clean.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm -r tmp_*
+rm *_head_walkcycle.png
+rm *_head_shadow_walkcycle.png
